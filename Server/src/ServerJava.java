d3600c44354d6ca67f29c32e9289d8158ee2f3ac
SauvanPL <sauvan@o2.pl>
2019-05-19 14:02:00 +0100

    Initial Commit

---
+++ Server/src/ServerJava.java
@@ -0,0 +1,16 @@
+
+public class ServerJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//changes 1
+//changes 2
+		//changes 3
+		//changes 4
+		//changes 5
+
+
+	}
+
+}
